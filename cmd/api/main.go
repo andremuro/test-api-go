a381daf0720fb792101e8dc96f09473a61248296
andremuro <andrefelipe.muro@gmail.com>
2023-10-26 19:44:47 -0300

    feat: :sparkles: Criando uma API simples em Go
    O intuito desse projeto é para treinar a criação e consumo de API's com GoLang
    
    Referência: https://www.youtube.com/watch?v=9BeFJuzg_yw

---
+++ cmd/api/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	//Definindo objeto do gin
+	g := gin.Default()
+	//Definindo rota (endpoint)
+	g.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+
+			"error":   "null",
+			"message": "Hello World!",
+		})
+	})
+	g.Run(":3001")
+}
