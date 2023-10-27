5295e69e1d0417b1e0d2b12a1156f97b82cd563a
andremuro <andrefelipe.muro@gmail.com>
2023-10-26 21:06:31 -0300

    feat: :sparkles: Adicionando conexão com Db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+
+	dbConfig "github.com/andremuro/test-api-go/postgres/dbconfig"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+var err error
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
 
 func main() {
 	//Definindo objeto do gin
+
+	fmt.Printf("Accessing %s ... ", dbConfig.DbName)
+	db, err = sql.Open(dbConfig.PostgresDriver, dbConfig.DataSourceName)
+
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("Connected!")
+	}
+	defer db.Close()
+
 	g := gin.Default()
 	//Definindo rota (endpoint)
 	g.GET("/", func(ctx *gin.Context) {
@@ -13,5 +41,5 @@ func main() {
 			"message": "Hello World!",
 		})
 	})
-	g.Run(":3001")
+	g.Run(":3002")
 }
